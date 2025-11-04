37ecde2959b93169e50b3ed23ac422e04b801ec7
dani784601 <dani784601@gmail.com>
2025-11-04 13:19:30 +0900

    feat: toast 메세지 라이브러리 설치

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,13 +2,16 @@ import { queryClient } from "@/api/queryClient";
 import useAuth from "@/hooks/queries/useAuth";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import Toast from "react-native-toast-message";
 
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaProvider>
         <RootNavigator />
+        <Toast />
       </SafeAreaProvider>
     </QueryClientProvider>
   );
@@ -16,7 +19,13 @@ export default function RootLayout() {
 
 function RootNavigator() {
   const { auth } = useAuth();
-  console.log(auth);
+  useEffect(() => {
+    auth.id &&
+      Toast.show({
+        type: "success",
+        text1: `${auth.id ?? "회원"}님 환영합니다!`,
+      });
+  }, [auth.id]);
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
